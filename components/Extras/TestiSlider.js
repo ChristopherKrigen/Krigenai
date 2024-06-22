34191648068a423e3ff63587da484d73cb9005a1
Ghufran Ullah <92268694+Ghufran496@users.noreply.github.com>
2024-06-22 21:18:36 +0500

    6th commit

--- components/Extras/TestiSlider.js
+++ components/Extras/TestiSlider.js
@@ -1,10 +1,8 @@
 import classes from "./TestiSlider.module.css";
 import Image from "next/image";
-import c1 from "../../images/c1.jpg";
-import c2 from "../../images/c2.jpg";
-import c3 from "../../images/c3.jpg";
-import c33 from "../../images/c33.jpg";
-// import p4 from "../../images/p4.webp";
+// import c1 from "../../images/c1.jpg";
+// import c2 from "../../images/c2.jpg";
+// import c3 from "../../images/c3.jpg";
 
 import coma from "../../images/comas.svg";
 
@@ -12,8 +10,15 @@ const TestimonialsSlider = () => {
   return (
     <div className="my-16 overflow-hidden">
       <div className={classes.imagecontainer}>
-        <div className="flex gap-14 w-8/12 bg-slate-50 rounded-3xl px-4 pt-6 pb-6 overflow-hidden">
-          <Image
+        <div className="flex gap-14 w-8/12 bg-slate-50 rounded-3xl px-4 py-2 overflow-hidden">
+          <div className="flex justify-center items-center">
+            <img
+              src="/c1.jpg"
+              alt="first service user"
+              style={{ borderRadius: "100%" }}
+            ></img>
+          </div>
+          {/* <Image
             src={c1}
             alt="Picture of Slack logo"
             className="rounded-full"
@@ -21,15 +26,15 @@ const TestimonialsSlider = () => {
             height={70}
             quality={100}
             sizes={"100vw"}
-          />
+          /> */}
 
           <div className="flex flex-col gap-16 pt-20">
-          <div className="flex justify-center items-center">
-            <p className="italic font-semibold text-xl text-center w-10/12">
-              “Krigen.ai automated aspects of my business that I never thought
-              could be automated. I have also saved a significant amount of
-              money on outsourced work.”
-            </p>
+            <div className="flex justify-center items-center">
+              <p className="italic font-semibold text-xl text-center w-10/12">
+                “Krigen.ai automated aspects of my business that I never thought
+                could be automated. I have also saved a significant amount of
+                money on outsourced work.”
+              </p>
             </div>
             <div className="flex justify-between">
               <div>
@@ -48,8 +53,15 @@ const TestimonialsSlider = () => {
           </div>
         </div>
 
-        <div className="flex gap-14 w-8/12 bg-slate-50 rounded-3xl px-4 pt-6 pb-6 overflow-hidden">
-          <Image
+        <div className="flex gap-14 w-8/12 bg-slate-50 rounded-3xl px-4 py-4 overflow-hidden">
+          <div className="flex justify-center items-center">
+            <img
+              src="/c2.jpg"
+              alt="second service user"
+              style={{ borderRadius: "100%" }}
+            ></img>
+          </div>
+          {/* <Image
             src={c2}
             alt="Picture of Slack logo"
             className="rounded-full"
@@ -57,14 +69,15 @@ const TestimonialsSlider = () => {
             height={70}
             quality={100}
             sizes={"100vw"}
-          />
+          /> */}
 
           <div className="flex flex-col gap-16 pt-20">
-          <div className="flex justify-center items-center">
-            <p className="italic font-semibold text-xl text-center w-10/12">
-              “Automating all my business operations with this software has been
-              a game changer. It is user-friendly and incredibly effective.”
-            </p>
+            <div className="flex justify-center items-center">
+              <p className="italic font-semibold text-xl text-center w-10/12">
+                “Automating all my business operations with this software has
+                been a game changer. It is user-friendly and incredibly
+                effective.”
+              </p>
             </div>
             <div className="flex justify-between">
               <div>
@@ -83,8 +96,15 @@ const TestimonialsSlider = () => {
           </div>
         </div>
 
-        <div className="flex gap-14 w-8/12 bg-slate-50 rounded-3xl px-4 pt-6 pb-6 overflow-hidden">
-          <div>
+        <div className="flex gap-14 w-8/12 bg-slate-50 rounded-3xl px-4 py-4 overflow-hidden">
+          <div className="flex justify-center items-center">
+            <img
+              src="/c3.jpg"
+              alt="third service user"
+              style={{ borderRadius: "100%" }}
+            ></img>
+          </div>
+          {/* <div>
             <Image
               src={c3}
               alt="Picture of Slack logo"
@@ -94,13 +114,13 @@ const TestimonialsSlider = () => {
               quality={100}
               sizes={"100vw"}
             />
-          </div>
+          </div> */}
           <div className="flex flex-col gap-16 pt-20">
             <div className="flex justify-center items-center">
-            <p className="italic font-semibold text-xl text-center w-10/12">
-              “The team at Krigen.ai has greatly streamlined how we operate.
-              Their communication was top-notch and we are very satisfied!”
-            </p>
+              <p className="italic font-semibold text-xl text-center w-10/12">
+                “The team at Krigen.ai has greatly streamlined how we operate.
+                Their communication was top-notch and we are very satisfied!”
+              </p>
             </div>
             <div className="flex justify-between">
               <div>
